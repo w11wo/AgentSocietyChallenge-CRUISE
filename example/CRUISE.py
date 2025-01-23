6485d2c18bfc92adacd6c002acf669b0a51b3a32
w11wo <wilsonwong961@gmail.com>
2025-01-23 17:32:26 +1100

    Added platform-specific prompt

--- example/CRUISE.py
+++ example/CRUISE.py
@@ -1,7 +1,7 @@
 from websocietysimulator.agent import SimulationAgent
 from websocietysimulator.llm import LLMBase
 from websocietysimulator.agent.modules.planning_modules import PlanningBase
-from websocietysimulator.agent.modules.reasoning_modules import ReasoningCOTSC
+from websocietysimulator.agent.modules.reasoning_modules import ReasoningBase, ReasoningTOT
 from websocietysimulator.agent.modules.memory_modules import MemoryGenerative
 
 
@@ -29,6 +29,27 @@ class PlanningBaseline(PlanningBase):
         return self.plan
 
 
+class CausalRating(ReasoningBase):
+    def __call__(self, task_description: str, initial_user_prompt: str, assistant_response: str, feedback: str = ""):
+        prompt = """Format your response exactly as follows:
+rating: [your rating]
+
+Here is the task:
+{task_description}"""
+        prompt = prompt.format(task_description=task_description)
+        # messages = [{"role": "user", "content": prompt}]
+        messages = [
+            {"role": "user", "content": initial_user_prompt},
+            {"role": "assistant", "content": assistant_response},
+            {"role": "user", "content": prompt},
+        ]
+        result = self.llm(
+            messages=messages,
+            temperature=0.0,
+        )
+        return result
+
+
 class MySimulationAgent(SimulationAgent):
     """Participant's implementation of SimulationAgent."""
 
@@ -36,7 +57,8 @@ class MySimulationAgent(SimulationAgent):
         """Initialize MySimulationAgent"""
         super().__init__(llm=llm)
         self.planning = PlanningBaseline(llm=self.llm)
-        self.reasoning = ReasoningCOTSC(profile_type_prompt="", memory=None, llm=self.llm)
+        self.reasoning = ReasoningTOT(profile_type_prompt="", memory=None, llm=self.llm)
+        # self.causal_thinking = CausalRating(profile_type_prompt="", memory=None, llm=self.llm)
         self.memory = MemoryGenerative(llm=self.llm)
 
     def workflow(self):
@@ -50,7 +72,11 @@ class MySimulationAgent(SimulationAgent):
 
             for sub_task in plan:
                 if "user" in sub_task["description"]:
-                    user = str(self.interaction_tool.get_user(user_id=self.task["user_id"]))
+                    user = self.interaction_tool.get_user(user_id=self.task["user_id"])
+                    if "friends" in user:
+                        del user["friends"]  # remove friends list for Yelp reviews
+                    platform = user["source"].capitalize()
+                    user = str(user)
                 elif "business" in sub_task["description"]:
                     business = str(self.interaction_tool.get_item(item_id=self.task["item_id"]))
             reviews_item = self.interaction_tool.get_reviews(item_id=self.task["item_id"])
@@ -60,7 +86,7 @@ class MySimulationAgent(SimulationAgent):
             reviews_user = self.interaction_tool.get_reviews(user_id=self.task["user_id"])
             review_similar = self.memory(f'{reviews_user[0]["text"]}')
             task_description = f"""
-            You are a real human user on Yelp, a platform for crowd-sourced business reviews. Here is your Yelp profile and review history: {user}
+            You are a real human user on {platform}, a platform for crowd-sourced business reviews. Here is your {platform} profile and review history: {user}
 
             You need to write a review for this business: {business}
 
@@ -84,6 +110,11 @@ class MySimulationAgent(SimulationAgent):
             - Focus on specific details about the business rather than generic comments
             - Be generous with ratings when businesses deliver quality service and products
             - Be critical when businesses fail to meet basic standards
+
+            Important:
+            - Reviews on Yelp and Goodreads are very conservative. User tend to only give a star rating of 2, 3, or 4. A 5-star rating is very rare. Only give a 5-star rating if the business is truly exceptional.
+            - Reviews on Amazon are more generous. Users tend to give 4 or 5-star ratings. A 3-star rating is considered a negative review on Amazon.
+            - Consider which platform you are on when writing your review. You are writing a review on {platform}.
 
             Format your response exactly as follows:
             stars: [your rating]
@@ -112,10 +143,16 @@ class MySimulationAgent(SimulationAgent):
 if __name__ == "__main__":
     import os
     import json
+    from argparse import ArgumentParser
     from websocietysimulator import Simulator
     from vllm import vLLM
 
-    os.makedirs("./example/results", exist_ok=True)
+    parser = ArgumentParser()
+    parser.add_argument("--dataset", type=str, choices=["amazon", "yelp", "goodreads"], default="amazon")
+    parser.add_argument("--exp_name", type=str, default="baseline")
+    args = parser.parse_args()
+
+    os.makedirs(f"./example/results_{args.exp_name}", exist_ok=True)
 
     agent_class = MySimulationAgent
     llm = vLLM(api_key=os.getenv("VLLM_API_KEY"))
@@ -124,20 +161,22 @@ if __name__ == "__main__":
     simulator.set_agent(agent_class)
     simulator.set_llm(llm)
 
-    for task_set in ["amazon", "goodreads", "yelp"]:
-        simulator.set_task_and_groundtruth(
-            task_dir=f"./example/track1/{task_set}/tasks", groundtruth_dir=f"./example/track1/{task_set}/groundtruth"
-        )
-        outputs = simulator.run_simulation(number_of_tasks=100, enable_threading=True, max_workers=8)
-        evaluation_results = simulator.evaluate()
+    simulator.set_task_and_groundtruth(
+        task_dir=f"./example/track1/{args.dataset}/tasks", groundtruth_dir=f"./example/track1/{args.dataset}/groundtruth"
+    )
+    outputs = simulator.run_simulation(number_of_tasks=100, enable_threading=True, max_workers=8)
+    evaluation_results = simulator.evaluate()
 
-        # dummy agent for logging purposes only
-        _agent = agent_class(llm=llm)
-        evaluation_results["agent_signature"] = {
-            "planning": _agent.planning.__class__.__name__,
-            "reasoning": _agent.reasoning.__class__.__name__,
-            "memory": _agent.memory.__class__.__name__,
-        }
+    # close cache database
+    del simulator.interaction_tool
 
-        with open(f"./example/results/evaluation_results_track1_{task_set}.json", "w") as f:
-            json.dump(evaluation_results, f, indent=4)
+    # dummy agent for logging purposes only
+    _agent = agent_class(llm=llm)
+    evaluation_results["agent_signature"] = {
+        "planning": _agent.planning.__class__.__name__,
+        "reasoning": _agent.reasoning.__class__.__name__,
+        "memory": _agent.memory.__class__.__name__,
+    }
+
+    with open(f"./example/results_{args.exp_name}/evaluation_results_track1_{args.dataset}.json", "w") as f:
+        json.dump(evaluation_results, f, indent=4)
